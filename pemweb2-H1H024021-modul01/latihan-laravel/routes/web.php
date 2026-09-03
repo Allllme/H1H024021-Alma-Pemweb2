78341c8f6fed9ee313bdd06e15f117bfca00d5d7
Allllme <almamaida0221@gmail.com>
2026-09-03 17:41:20 +0700

    Modul 1: struktur folder pemweb2-H1H024021-modul01 (Laravel dan Fiber)

---
+++ pemweb2-H1H024021-modul01/latihan-laravel/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return '<h1>Alma Maida Wirastuti</h1><p>NIM: H1H024021</p><p>Teknik Komputer - Pemrograman Web II</p>';
+});
