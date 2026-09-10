fc670850c7c8f3ade1abaa5c61124738f44f2abb
Allllme <almamaida0221@gmail.com>
2026-09-10 23:03:57 +0700

    Modul 2: routing, controller, dan Blade (Langkah 1-11)

--- pemweb2-H1H024021-modul01/latihan-laravel/routes/web.php
+++ pemweb2-H1H024021-modul01/latihan-laravel/routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MahasiswaController;
+
 Route::get('/', function () {
     return '<h1>Alma Maida Wirastuti</h1><p>NIM: H1H024021</p><p>Teknik Komputer - Pemrograman Web II</p>';
 });
+
+Route::get('/salam', function () {
+    return 'Selamat datang di Pemrograman Web II';
+});
+Route::get('/mahasiswa/{nim}', function (string $nim) {
+    return 'Data mahasiswa dengan NIM ' . $nim;
+});
+
+Route::get('/matakuliah/{kode?}', function (?string $kode = null) {
+    if ($kode === null) {
+        return 'Menampilkan seluruh matakuliah';
+    }
+    return 'Menampilkan matakuliah kode ' . $kode;
+});
+Route::get('/semester/{angka}', function (int $angka) {
+    return 'Semester ke ' . $angka;
+})->whereNumber('angka');
+
+Route::get('/data-mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+Route::get('/data-mahasiswa/{nim}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
+Route::get('/cari-mahasiswa', [MahasiswaController::class, 'cari']);
