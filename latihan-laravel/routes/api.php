2a2dd78d5c0f48685a9d68699339efda7807f653
Alma Maida Wirastuti <almamaida0221@gmail.com>
2026-09-26 03:01:57 +0700

    Modul 4: RESTful API dengan Laravel (Langkah 1-11 + Tugas Praktikum)

---
+++ latihan-laravel/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\MatakuliahController;
+use App\Http\Controllers\Api\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/status', function () {
+    return response()->json([
+        'sukses' => true,
+        'pesan' => 'API Pemweb II aktif',
+        'waktu' => now()->toIso8601String(),
+    ]);
+});
+
+Route::apiResource('mahasiswa', MahasiswaController::class);
+Route::apiResource('matakuliah', MatakuliahController::class);
+Route::get('/program-studi/{programStudi}/mahasiswa', [MahasiswaController::class, 'perProgramStudi']);
